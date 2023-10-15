ec4e7c017fcfdf1ef81a08e6f1a88b4ad8d701da
TinaZohouri <Tinazohouty80@gmail.com>
2023-10-16 00:50:50 +0330

    Finish Project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
-/*=============== SHOW MENU ===============*/
 
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-/*=============== REMOVE MENU MOBILE ===============*/
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
+const navLink = document.querySelectorAll(".nav__link");
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 80) {
+    header.classList.add("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/
-
-
-/*=============== DARK LIGHT THEME ===============*/
